8c2e0c06e2ca2fad48091ae57bcb082ce0204672
dylanh6 <dylanh6@vt.edu>
2017-12-03 18:10:11 -0500

    readded input

---
+++ TempProjectRunner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author David
+ *
+ */
+public class TempProjectRunner {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        GUIDisplayWindow window = new GUIDisplayWindow();
+    }
+
+}
